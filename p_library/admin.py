20849a5fd6574aa04bfee9084ddc6b7d8ea62b12
uuso <mymail.uuso@gmail.com>
2019-11-17 01:00:45 +0300

    added superuser and admin models [D4.6]

--- p_library/admin.py
+++ p_library/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from p_library.models import Book, Author
 
 # Register your models here.
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+# @admin.register(Author)
+# class AuthorAdmin(admin.ModelAdmin):
+#     pass
+
+#Альтернативная запись, т.к. в предыдущем виде внутри класса ничего не выполняется (pass)
+admin.site.register(Author)
