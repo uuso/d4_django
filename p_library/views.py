f72e10c84610497f7b808b0a87865392430c5a53
uuso <mymail.uuso@gmail.com>
2019-11-17 18:39:06 +0300

    edit: some pylint recommendations

--- p_library/views.py
+++ p_library/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, redirect
-from p_library.models import Book, Author
+from django.shortcuts import redirect
 from django.http import HttpResponse
 from django.template import loader
+from p_library.models import Book
 
 def index(request):
     template = loader.get_template('index.html')
