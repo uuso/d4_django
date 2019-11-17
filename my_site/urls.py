a5d5f4a9343d7950cf805f724b31d485979ce0e0
uuso <mymail.uuso@gmail.com>
2019-11-17 15:07:42 +0300

    added: ability to inc/dec copy_count

--- my_site/urls.py
+++ my_site/urls.py
@@ -20,4 +20,6 @@ from p_library import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index),
+    path('index/book_increment/', views.book_increment),
+    path('index/book_decrement/', views.book_decrement),
 ]
--- p_library/views.py
+++ p_library/views.py
@@ -14,3 +14,28 @@ def index(request):
     return HttpResponse(template.render(biblio_data, request))
 
 
+def book_increment(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if book_id:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            book.copy_count += 1
+            book.save()    
+    return redirect('/index/')
+
+def book_decrement(request):
+    if request.method == 'POST':
+        book_id = request.POST['id']
+        if book_id:
+            book = Book.objects.filter(id=book_id).first()
+            if not book:
+                return redirect('/index/')
+            if book.copy_count < 1:
+                book.copy_count = 0
+            else:
+                book.copy_count -= 1
+            book.save()    
+    return redirect('/index/')
+        
