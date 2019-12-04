88b18d690be8dda6c84f278201f7787973017a0a
uuso <mymail.uuso@gmail.com>
2019-12-04 22:12:02 +0300

    heroku strikes again

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 release: python manage.py migrate
-release: python manage.py loaddata data_p_library.xml
 web: python manage.py runserver 0.0.0.0:$PORT
--- my_site/settings.py
+++ my_site/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-# DATABASES = {
+#DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
